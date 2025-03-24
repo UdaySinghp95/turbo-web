15323631ffa26e2f0065a92203e0bb8f25552ab5
“Uday” <uday.singh@pocketpills.com>
2025-03-25 01:12:09 +0530

    Code update

--- apps/pocketpills/next.config.mjs
+++ apps/pocketpills/next.config.mjs
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/blog',
-                destination: `${process.env.BLOG}`,
-            },
-            {
-                source: '/blog/:path+',
-                destination: `${process.env.BLOG}/blog/:path+`,
-            },
-        ];
-    }
 };
 
 export default nextConfig;
